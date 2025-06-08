8095f603452ac64cbfbd76ace3ed58fa5c397432
baishyaRahul <baishyarahul94@gmail.com>
2025-06-08 12:44:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+function displayResume() {
+    return `=== RESUME DETAILS ===
+
+Rahul Baishya
+Software Developer @ NIC, India
+Skills: HTML, CSS, Bootstrap, Laravel, PHP, JavaScript, GitHub, Linux, MySQL, PostgreSQL
+
+📍 Location:
+Guwahati, Assam, India
+
+📧 Contact:
+rahulbaishya8@gmail.com
+
+🔗 LinkedIn:
+www.linkedin.com/in/rahul-baishya-412785167
+
+🛠️ Top Skills:
+- jQuery
+- JavaScript
+- Git
+
+📜 Certifications:
+- SQL
+
+💼 Experience:
+
+National Informatics Centre, Govt of India
+- Software Developer (May 2024 – Present) – 1 year 2 months
+- Software Application Support Engineer (June 2022 – September 2024) – 2 years 4 months
+
+Gratia Technology Private Limited
+- Project Associate (July 2021 – June 2022) – 1 year
+
+🎓 Education:
+
+Jorhat Engineering College
+- Master of Computer Applications (2017 – 2020)
+
+Gauhati University
+- Bachelor of Science - BS, Computer Science (August 2014 – September 2017)
+`;
+}
+
+
+module.exports = displayResume;
